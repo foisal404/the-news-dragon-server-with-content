d319d84c0df6aa17b8f5a8bb662ab94725e1afa2
foisal404 <foisalahmmed2@gmail.com>
2023-05-03 17:07:54 +0600

    vercel json added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news=require('./data/news.json')
 
 app.use(cors());
 
@@ -11,10 +12,34 @@ app.get('/', (req, res) =>{
     res.send('Dragon is running')
 });
 
+//all catagores
 app.get('/categories', (req, res) =>{
-    console.log(categories);
+    // console.log(categories);
     res.send(categories);
 })
+//all news
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+
+//catagory with specific id
+app.get('/categories/:id',(req,res)=>{
+    const id=req.params.id;
+    // console.log(id);
+    if(id==='0'){
+        res.send(news)
+    }
+    else{
+        res.send(news.filter(cate=>cate.category_id===id))
+    }
+})
+
+//news with specific id
+app.get('/news/:id',(req,res)=>{
+    const id=req.params.id;
+    res.send(news.find(cata=>cata._id===id))
+})
+
 
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
